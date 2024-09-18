1796d0248d508ef8ea57211a81a538beec90fc38
Tomas Kallup <t.kallup@gmail.com>
2024-09-18 17:04:40 +0200

    Use raylib5 from vendor-fetch, port simple example
    Switched to raylib5 from vendor-fetch, which required some Nix magic
    to have proper glfw3.so file.
    
    Port simple example from raylib c examples, with custom modifications:
     - mouse camera panning
     - work in "top-down" view
     - game object base
    https://github.com/raysan5/raylib/blob/master/examples/core/core_2d_camera.c

--- devenv.nix
+++ devenv.nix
@@ -1,17 +1,25 @@
-{ pkgs, lib, config, inputs, ... }:
+{ pkgs, ... }:
 
+let
+  custom-glfw = pkgs.glfw.overrideAttrs (old: {
+    postInstall = (old.postInstall or "") + ''
+      ln -s $out/lib/libglfw.so $out/lib/libglfw3.so
+    '';
+  });
+in
 {
 
-  env.RAYLIB_DIR = "${pkgs.raylib}";
   # https://devenv.sh/packages/
-  packages = with pkgs; [ git c3c raylib glfw gcc ];
+  packages = with pkgs; [
+    git
+    c3c
+    custom-glfw
+  ];
 
-  enterShell = ''
-  '';
+  enterShell = '''';
 
   # https://devenv.sh/tests/
-  enterTest = ''
-  '';
+  enterTest = '''';
 
   # See full reference at https://devenv.sh/reference/options/
 }
